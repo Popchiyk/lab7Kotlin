860bcaf74ff508938e75b225d7abb3f4c0397f32
Popchiyk <roma240103@gmail.com>
2023-04-03 23:24:59 +0300

    improve my code

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -70,7 +70,7 @@ class SleepTrackerFragment : Fragment() {
         // To use the View Model with data binding, you have to explicitly
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
-                adapter.data = it
+                adapter.bind = it
             }
         })
         // give the binding object a reference to it.
